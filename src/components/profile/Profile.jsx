72eb8112a94e88919594bb4da3cba02d4697dd21
NataliaSilantieva <silantieva8888@gmail.com>
2022-06-15 11:27:24 +0300

    Create Profile.jsx

---
+++ src/components/profile/Profile.jsx
@@ -0,0 +1,49 @@
+
+import PropTypes from 'prop-types';
+import s from './Profile.module.css';
+
+const Profile = ( {username = 'unknown' , tag, location, avatar, stats} ) => {
+    return (
+      <div className={s.profile}>
+    <div className="description">
+      <img
+        src={avatar}
+        alt={username}
+        className={s.avatar}
+      />
+      <p className={s.username}>{username}</p>
+      <p className={s.tag}>@{tag}</p>
+      <p className="location">{location}</p>
+    </div>
+  
+    <ul className={s.stats}>
+      <li>
+        <span className="label">Followers</span>
+        <span className="quantity">{stats.followers}</span>
+      </li>
+      <li>
+        <span className="label">Views</span>
+        <span className="quantity">{stats.views}</span>
+      </li>
+      <li>
+        <span className="label">Likes</span>
+        <span className="quantity">{stats.likes}</span>
+      </li>
+    </ul>
+  </div>
+    );
+  };
+  
+  Profile.propTypes = {
+    username: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    stats: PropTypes.shape({
+      followers: PropTypes.string.isRequired,
+      likes: PropTypes.string.isRequired,
+      views: PropTypes.string.isRequired,
+    })
+  };
+  
+  export default Profile;
