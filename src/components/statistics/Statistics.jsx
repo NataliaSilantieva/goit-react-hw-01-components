af1a21e34267ad771908f6462bffee862938cf46
NataliaSilantieva <silantieva8888@gmail.com>
2022-06-24 22:20:45 +0300

    Update Statistics.jsx

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import s from './Statistics.module.css';
 
-const Stats = ({ title, stats }) => {
+const Statistics = ({ title, stats }) => {
   return (
     <section className={s.stats}>
       {title && <h2 className={s.title}>{title}</h2>}
@@ -18,11 +18,11 @@ const Stats = ({ title, stats }) => {
   );
 };
 
-Stats.propTypes = {
+Statistics.propTypes = {
   title: PropTypes.string.isRequired,
   stats: PropTypes.shape({
     label: PropTypes.string.isRequired,
     percentage: PropTypes.number.isRequired,
   }),
 };
-export default Stats;
+export default Statistics;
