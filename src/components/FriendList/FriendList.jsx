64d8c1608cf82d663dc6928405926829c42736a8
NataliaSilantieva <silantieva8888@gmail.com>
2022-06-24 18:40:19 +0300

    Create FriendList.jsx

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+import FriendListItem from './FriendListItem';
+import s from './FriendList.module.css';
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul className={s.friendList}>
+      {friends.map(friend => (
+        <li className={s.item} key={friend.id}>
+          <FriendListItem
+            avatar={friend.avatar}
+            name={friend.name}
+            isOnline={friend.isOnline}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }),
+  ),
+};
+
+export default FriendList;
