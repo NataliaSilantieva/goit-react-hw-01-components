e258fcc0c96a1fed55d20e745a9ca64629671639
NataliaSilantieva <silantieva8888@gmail.com>
2022-06-24 18:40:11 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,17 @@ import Container from './components/container/Container.js';
 import user from './components/database/user.json'; 
 import Profile from './components/profile/Profile.jsx';
 
+
+import Statistics from './components/statistics/Statistics';
+import data from './components/database/data.json';
+
+import FriendList from './components/FriendList/FriendList';
+import friends from './components/database/friends.json';
+
+import TransactionHistory from './components/transactionHistory/TransactionHistory';
+import transactions from './components/database/transactions.json';
+
+
 export default function App() {
   return (
     <Container>
@@ -12,7 +23,10 @@ export default function App() {
   avatar={user.avatar}
   stats={user.stats}
 />
-
+<Statistics title="Upload stats" Stats={data} />
+      <Statistics stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
 
     </Container>
   );
